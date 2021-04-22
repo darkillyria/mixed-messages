188830dc16dee01abbe3f34fec581f1830095079
Dani <darkillyria@yahoo.com>
2021-04-21 22:00:17 -0400

    carjs.js added

---
+++ carjs.js
@@ -0,0 +1,3 @@
+const color = [red, green, blue, white, black, gray, brown, pink, purple, orange, yellow, tan];
+
+const make = [BMW, Chevy, GMC, Subaru, Buick, Ford, Dodge, Toyota, Nissan, Honda, Volvo, Kia, Hyundai]
